86c5329ef434e1ebec37b460b2f0f198edbcdd39
Phuc Tran <phuc.d.tran.24@dartmouth.edu>
2023-10-12 22:26:13 -0400

    pushing packages and whatnot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let x = 0;
+function counter() {
+  x += 1;
+  $('#main').html(`You have been here for ${x.toString()} seconds`);
+}
+
+setInterval(counter, 1000);
